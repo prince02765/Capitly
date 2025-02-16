2cbdd0384067a489f0e0490a087b278affae0995
Prince Patel <prince90290.dev@gmail.com>
2025-02-16 13:50:06 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/capitly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.capitly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Capitly",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Capitly"),
+      ),
+      body: Center(child: Text("Welcome to Capitly!")),
+    );
+  }
+}
